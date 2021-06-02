2be281d8f7ece4bac0d4d8417475e15154a38fbe
akshat <akshat_666@yahoo.com>
2021-06-02 23:31:27 +0530

    Issue with ionic radio selection

---
+++ src/views/Home.vue
@@ -0,0 +1,85 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Radio issue</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-list>
+        <ion-radio-group>
+          <ion-item v-for="it in myitems" :key="it.id">
+            <ion-label>{{ it.name }}</ion-label>
+            <ion-radio value="it.id"></ion-radio>
+          </ion-item>
+        </ion-radio-group>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonLabel,
+  IonList
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonLabel,
+    IonList,
+  },
+  data() {
+    return {
+      myitems: [
+        { id: 1, name: "item01" },
+        { id: 2, name: "item02" },
+        { id: 3, name: "item03" },
+      ],
+    };
+  },
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+
+  color: #8c8c8c;
+
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
